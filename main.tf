b4775663182485cc67981102b6c3128649d3bdb9
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-08-28 15:31:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,15 @@ provider azurerm {
   features {}
 }
 
-resource "azurerm_resource_group" "resourcestorage" {
+resource "azurerm_resource_group" "resource_storage" {
   name     = "resource_storage"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "storage_pratice" {
   name                     = "terraformstorageaccount11"
-  resource_group_name      = azurerm_resource_group.StorageApplication.name
-  location                 = azurerm_resource_group.StorageApplication.location
+  resource_group_name      = azurerm_resource_group.resource_storage.name
+  location                 = azurerm_resource_group.resource_storage.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
