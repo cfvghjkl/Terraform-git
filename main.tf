9839fe6e39ac57328d414ba5d0caa82d5960aebe
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-08-28 15:38:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "StorageApplication" {
-  name     = "StorageApplication"
-  location = "East US"  # Replace with your desired location
+resource "azurerm_resource_group" "Storage" {
+  name     = "Storage"
+  location = "East US"  
 }
 
 resource "azurerm_storage_account" "storage_pratice" {
   name                     = "storagepratice"
-  resource_group_name      = azurerm_resource_group.StorageApplication.name
-  location                 = azurerm_resource_group.StorageApplication.location
+  resource_group_name      = azurerm_resource_group.Storage.name
+  location                 = azurerm_resource_group.Storage.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
